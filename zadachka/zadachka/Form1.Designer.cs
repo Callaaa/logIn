b9776fcee23c43eaec7303350fc5e91798490872
Callaaa <148789803+Callaaa@users.noreply.github.com>
2024-12-11 11:40:13 +0200

    Add files via upload

---
+++ zadachka/zadachka/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace zadachka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtEGN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblIzhod = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(33, 96);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtEGN
+            // 
+            this.txtEGN.Location = new System.Drawing.Point(33, 151);
+            this.txtEGN.Name = "txtEGN";
+            this.txtEGN.Size = new System.Drawing.Size(100, 20);
+            this.txtEGN.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Първо име:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(29, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ЕГН:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(29, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Изход:";
+            // 
+            // lblIzhod
+            // 
+            this.lblIzhod.AutoSize = true;
+            this.lblIzhod.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblIzhod.Location = new System.Drawing.Point(111, 205);
+            this.lblIzhod.Name = "lblIzhod";
+            this.lblIzhod.Size = new System.Drawing.Size(0, 24);
+            this.lblIzhod.TabIndex = 5;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(154, 82);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(104, 46);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Проверка";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(154, 137);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(104, 46);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Затвори";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 284);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblIzhod);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEGN);
+            this.Controls.Add(this.txtIme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtEGN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblIzhod;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ zadachka/zadachka/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadachka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                char bukva = txtIme.Text.Trim().ToLower()[0];
+                string egn = txtEGN.Text;
+                lblIzhod.Text = $"{txtIme.Text} {txtEGN.Text}";
+                MessageBox.Show($"{bukva},{egn[6]},{egn[7]}", "ime");
+
+                int position = 1;             
+                for (char b = 'a'; b < 'z'; b++)
+                {
+                    if (b == bukva)
+                    {
+                        break;
+                    }
+                    else { position++; }                   
+                }
+                if (position == egn[6] - '0'+ egn[7] - '0')
+                {
+                    MessageBox.Show("suvpadat");
+                }
+                else
+                {
+                    MessageBox.Show("ne suvpadat");
+                }
+            }
+            catch (ArgumentException)
+            {
+
+                Console.WriteLine("Greshka");
+            }
+                  
+        }
+    }
+}
